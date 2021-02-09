5d869e415f342f69024fff29c4cbe86075e22399
virginiegodfrin <virginiegodfrin@gmail.com>
2021-02-09 15:36:18 +0100

    chap06 - Hooks: setup, tearDown & Skipping Test

--- tests/AppBundle/Factory/DinosaurFactoryTest.php
+++ tests/AppBundle/Factory/DinosaurFactoryTest.php
@@ -7,11 +7,21 @@ use AppBundle\Factory\DinosaurFactory;
 
 class DinosaurFactoryTest extends TestCase
 {
+	/**
+     * @var DinosaurFactory
+     */
+    private $factory;
+
+    // If you have a method that's exactly called setUp, PHPUnit will automatically call it before each test.
+    // This will make sure that the $factory property is a new, fresh DinosaurFactory object for every test
+    public function setUp():void
+    {	
+        $this->factory = new DinosaurFactory();
+    }
 
 	public function testItGrowsALargeVelociraptor()
 	{
-		$factory = new DinosaurFactory();
-        $dinosaur = $factory->growVelociraptor(5);
+        $dinosaur = $this->factory->growVelociraptor(5);
 
         $this->assertInstanceOf(Dinosaur::class, $dinosaur);
         $this->assertInternalType('string', $dinosaur->getGenus());
@@ -19,5 +29,23 @@ class DinosaurFactoryTest extends TestCase
         $this->assertInternalType('bool', $dinosaur->isCarnivorous());
         $this->assertSame(true, $dinosaur->isCarnivorous());
         $this->assertSame(5, $dinosaur->getLength());
+	}
+
+	// A test to be completed later 
+	public function testItGrowsATriceraptors()
+    {
+        $this->markTestIncomplete('Waiting for confirmation from GenLab');
+    }
+
+    // Skipping test
+	public function testItGrowsABabyVelociraptor()
+	{
+		if (!class_exists('Nanny')) {
+            $this->markTestSkipped('There is nobody to watch the baby!');
+        }
+
+		$dinosaur = $this->factory->growVelociraptor(1);
+
+		$this->assertSame(1, $dinosaur->getLength());
 	}
 }
