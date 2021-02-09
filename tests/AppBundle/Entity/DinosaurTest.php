86c494c32463b3016504e00f03776c9283c30591
virginiegodfrin <virginiegodfrin@gmail.com>
2021-02-09 10:19:10 +0100

    Chap01: PHPUnit: Secure the Park

---
+++ tests/AppBundle/Entity/DinosaurTest.php
@@ -0,0 +1,18 @@
+<?php
+namespace Tests\AppBundle\Entity;
+
+use PHPUnit\Framework\TestCase;
+
+// Directory: Tests
+// Config: phpUnit.xml.dist
+// Namespace start with Tests\...
+// Test class must extends TestCase and end with Test: xxxTest
+// All the methods are public and strat with test : testXxx
+
+class DinosaurTest extends TestCase
+{
+	public function testThatYourComputerWorks()
+    {
+        $this->assertTrue(false);
+    }
+}
