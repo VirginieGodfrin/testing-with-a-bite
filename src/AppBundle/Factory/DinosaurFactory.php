e9fe80833e474eb1925bce076971d04192d17b27
virginiegodfrin <virginiegodfrin@gmail.com>
2021-02-11 10:32:31 +0100

    Chap12 - Refactoring & Dependency Injection

--- src/AppBundle/Factory/DinosaurFactory.php
+++ src/AppBundle/Factory/DinosaurFactory.php
@@ -2,9 +2,17 @@
 namespace AppBundle\Factory;
 
 use AppBundle\Entity\Dinosaur;
+use AppBundle\Service\DinosaurLengthDeterminator;
 
 class DinosaurFactory
 {
+    private $lengthDeterminator;
+
+    public function __construct(DinosaurLengthDeterminator $lengthDeterminator)
+    {
+        $this->lengthDeterminator = $lengthDeterminator;
+    }
+
     public function growVelociraptor(int $length): Dinosaur
     {
     	return $this->createDinosaur('Velociraptor', true, $length);
@@ -13,7 +21,7 @@ class DinosaurFactory
     public function growFromSpecification(string $specification): Dinosaur
     {
     	$codeName = 'InG-' . random_int(1, 99999);
-    	$length = $this->getLengthFromSpecification($specification);
+    	$length = $this->lengthDeterminator->getLengthFromSpecification($specification);
         $isCarnivorous = false;
 
         if (stripos($specification, 'carnivorous') !== false) {
@@ -31,29 +39,4 @@ class DinosaurFactory
         $dinosaur->setLength($length);
         return $dinosaur;
 	}
-
-	private function getLengthFromSpecification(string $specification): int
-    {
-        $availableLengths = [
-            'huge' => ['min' => Dinosaur::HUGE, 'max' => 100],
-            'omg' => ['min' => Dinosaur::HUGE, 'max' => 100],
-            '😱' => ['min' => Dinosaur::HUGE, 'max' => 100],
-            'large' => ['min' => Dinosaur::LARGE, 'max' => Dinosaur::HUGE - 1],
-        ];
-        $minLength = 1;
-        $maxLength = Dinosaur::LARGE - 1;
-
-        foreach (explode(' ', $specification) as $keyword) {
-            $keyword = strtolower($keyword);
-
-            if (array_key_exists($keyword, $availableLengths)) {
-                $minLength = $availableLengths[$keyword]['min'];
-                $maxLength = $availableLengths[$keyword]['max'];
-
-                break;
-            }
-        }
-
-        return random_int($minLength, $maxLength);
-    }
 }
---
+++ tests/AppBundle/Service/DinosaurLengthDeterminatorTest.php
@@ -0,0 +1,37 @@
+<?php
+namespace Tests\Service;
+
+use PHPUnit\Framework\TestCase;
+use AppBundle\Entity\Dinosaur;
+use AppBundle\Service\DinosaurLengthDeterminator;
+
+class DinosaurLengthDeterminatorTest extends TestCase
+{
+
+	/**
+     * @dataProvider getSpecLengthTests
+     */
+	public function testItReturnsCorrectLengthRange($spec, $minExpectedSize, $maxExpectedSize)
+	{
+		$determinator = new DinosaurLengthDeterminator();
+        $actualSize = $determinator->getLengthFromSpecification($spec);
+
+        $this->assertGreaterThanOrEqual($minExpectedSize, $actualSize);
+        $this->assertLessThanOrEqual($maxExpectedSize, $actualSize);
+	}
+
+	public function getSpecLengthTests()
+	{
+        return [
+            // specification, min length, max length
+            ['large carnivorous dinosaur', Dinosaur::LARGE, Dinosaur::HUGE - 1],
+            'default response' => ['give me all the cookies!!!', 0, Dinosaur::LARGE - 1],
+            ['large herbivore', Dinosaur::LARGE, Dinosaur::HUGE - 1],
+            ['huge dinosaur', Dinosaur::HUGE, 100],
+            ['huge dino', Dinosaur::HUGE, 100],
+            ['huge', Dinosaur::HUGE, 100],
+            ['OMG', Dinosaur::HUGE, 100],
+            ['😱', Dinosaur::HUGE, 100],
+        ];
+	}
+}
